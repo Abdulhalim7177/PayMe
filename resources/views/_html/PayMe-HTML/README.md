f7a79b6b947669e3c31a713a3d2d462179eb6d26
Abdulhalim7177 <abdulmuhd7177@gmail.com>
2025-01-16 13:40:08 +0100

    initial commit

---
+++ resources/views/_html/PayMe-HTML/README.md
@@ -0,0 +1,14 @@
+# PayMe HTML Template
+HTML template for PayMe SaaS application.
+
+
+## Deployment commands
+
+```
+npm install 
+npm run production
+```
+
+
+## MIT License
+(c) 2018 [Max Kostinevich](https://maxkostinevich.com) & [Laravel 101](https://laravel101.com) - All rights reserved.
---
+++ routes/web.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Auth::routes(['verify' => true]);
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
