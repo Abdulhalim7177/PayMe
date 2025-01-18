6960d92e22e0914386cb95525de10ce71fa97391
Abdulhalim7177 <abdulmuhd7177@gmail.com>
2025-01-18 15:27:54 +0100

    modify blade views

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,16 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', 'PageController@home')->name('page.home');
+
+Route::get('/terms', 'PageController@terms')->name('page.terms
+');
+Route::get('/privacy', 'PageController@privacy')->name('page.
+privacy');
 
 
 Auth::routes(['verify' => true]);
